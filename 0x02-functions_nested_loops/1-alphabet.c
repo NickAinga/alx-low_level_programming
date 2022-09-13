f64ea82b606148cc0ea4f696edf88075ab5dd08a
NickAinga <nickoluoch32@gmail.com>
2022-09-13 02:48:24 -0700

    added file 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+  *main -entry poin
+  *
+  *Return: 0 Success
+  */
+void print_alphabet(void)
+{
+	int d;
+
+	for (d = 'a'; d <= 'z'; d++)
+	{
+		_putchar(d);
+	}
+	_putchar('\n');
+}
