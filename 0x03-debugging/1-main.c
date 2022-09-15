9f7acc0f260e00348179a4534520cbca6924b797
NickAinga <nickoluoch32@gmail.com>
2022-09-15 02:05:52 -0700

    added file 1

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+  *main - cause an infinite loop
+  *Return: 0
+  */
+int mai(void)
+{
+	int i;
+
+	printf("Infinite loop incominf :(\n");
+
+	i = 0;
+	/**
+	  *while (i < 10)
+	  *{
+	  *	putchar(i);
+	  *}
+	  */
+printf("infinite loop avoided! \\o/\n");
+
+return (0);
+}
--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -2,3 +2,5 @@
 #define MAIN_H
 
 #include <stdio.h>
+
+void positive_or_negative(int i);
