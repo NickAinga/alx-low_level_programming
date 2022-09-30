609cd4cbda2fc2eaaf8fb2276b1998e0370b7ba6
NickAinga <nickoluoch32@gmail.com>
2022-09-30 05:16:15 -0700

    added file 0, Readme, and main

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+/**
+  *main - function to print name of function followed by newline
+  *@argc: unused argument
+  *
+  *@argv: argument whos first element [0] is the program name
+  *
+  *Return: (0)
+  */
+int main(int argc, char *argv[])
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
